e63e740a466abf6a828ee8a30dd72f629655bae6
Juan Cortes <jcortes@todosistemassti.co>
2024-06-22 09:52:04 -0500

    cambiando idioma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,13 @@
 <div class="card card-md">
     <div class="card-body">
         <h2 class="h2 text-center mb-4">
-            Login to your account
+            Ingreso
         </h2>
         <form action="{{ route('login') }}" method="POST" autocomplete="off">
             @csrf
             <div class="mb-3">
                 <label for="email" class="form-label">
-                    Email address
+                    Email
                 </label>
                 <input type="email"
                        name="email"
@@ -30,7 +30,7 @@
 
             <div class="mb-2">
                 <label for="password" class="form-label">
-                    Password
+                    Clave
                 </label>
 
                 <div class="input-group input-group-flat">
@@ -53,25 +53,25 @@
             <div class="mb-2">
                 <label for="remember" class="form-check">
                     <input type="checkbox" id="remember" name="remember" class="form-check-input"/>
-                    <span class="form-check-label">Remember me on this device</span>
+                    <span class="form-check-label">Recordar</span>
                 </label>
             </div>
 
             <div class="form-footer">
                 <button type="submit" class="btn btn-primary w-100">
-                    Sign in
+                    Ingresar
                 </button>
             </div>
         </form>
     </div>
 </div>
 <div class="text-center text-secondary mt-3">
-    Don't have account yet? <a href="{{ route('register') }}" tabindex="-1">
-        Sign up
+    No tiene cuenta? <a href="{{ route('register') }}" tabindex="-1">
+        Registrarse
     </a>
 
     <span class="form-label-description">
-        <a href="{{ route('password.request') }}">I forgot password</a>
+        <a href="{{ route('password.request') }}">Olvido la clave</a>
     </span>
 </div>
 @endsection
--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -49,7 +49,7 @@
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href="{{ url('/') }}">
-                        <img src="{{ asset('static/logo.svg') }}" width="110" height="32" alt="Tabler"
+                        <img src="{{ asset('static/logos.svg') }}" width="110" height="32" alt="Inventario"
                             class="navbar-brand-image">
                     </a>
                 </h1>
@@ -117,7 +117,7 @@
                                 {{--                                    </div> --}}
                                 <span class="dropdown-header">Dropdown header</span>
                                 <a class="dropdown-item" href="#">
-                                    Action
+                                    Accion
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     Another action
@@ -131,7 +131,7 @@
                                 <div class="dropdown-menu">
                                     <span class="dropdown-header">Dropdown header</span>
                                     <a class="dropdown-item" href="#">
-                                        Action
+                                        Accion
                                     </a>
                                     <a class="dropdown-item" href="#">
                                         Another action
@@ -198,7 +198,7 @@
                                         <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
                                         <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
                                     </svg>
-                                    Action
+                                    Accion
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon icon-tabler icon-tabler-pencil" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
